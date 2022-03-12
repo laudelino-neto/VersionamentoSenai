0ad8cc192a9ad3c392aba58fac7c3ae07c31c958
laudelino.cardoso <laudelino@farolshopping.com.br>
2022-03-12 15:03:02 -0300

    Inicio da criação da camada de repositório do aluno

---
+++ src/br/com/senai/versionamentosenai/repository/AlunosRepository.java
@@ -0,0 +1,9 @@
+package br.com.senai.versionamentosenai.repository;
+
+import java.io.Serializable;
+
+public class AlunosRepository implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
